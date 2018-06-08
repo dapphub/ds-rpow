3cd68411afe4cafd969cc496810977bff8744542
Mikael Brockman <mikael@brockman.se>
2018-06-08 21:44:34 +0300

    optimize x=0 case

--- src/DsRpow.t.sol
+++ src/DsRpow.t.sol
@@ -14,6 +14,7 @@ contract DsRpowTest is DSTest {
     function test_basic_sanity() public {
         // very test, much correctness
         uint base = 10**27;
+        assertEq(rpow.rpow(0, 3, base), 0);
         assertEq(rpow.rpow(10 * base, 3, base), 1000 * base);
         assertEq(rpow.rpow(10 * base, 4, base), 10000 * base);
         assertEq(rpow.rpow(3 * base, 13, base), 1594323 * base);
